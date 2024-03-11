0330a5493ceda375a9433de2b6df677f8f355346
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2024-03-11 12:02:45 +0100

    complete my skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,80 +1,77 @@
 import React from "react";
 import "../skills.css";
-import { Icon } from "@iconify/react";
+// import { Icon } from "@iconify/react";
+
 const Skills = () => {
   return (
     <>
-      <section className="skills-container d-grid justify-content-center align-items-center m-0">
-        <div className="">
-          <h1>My SKills</h1>
-        </div>
-        <div className="">
-          <div class="slider d-flex justify-content-center align-items-center py-4">
-            <div class="slider-items d-flex justify-content-center  align-items-center gap-5 text-center">
-              <div className="text-black d-grid justify-content-center align-items-center text-center">
-                <span className="skill-icons--html"></span> <span>HTML 5</span>
-              </div>
-              <div className="text-black d-grid justify-content-center align-items-center text-center">
-              <span class="skill-icons--css"></span> <span>CSS 3</span>
-              </div>
-              <div className="text-black d-grid justify-content-center align-items-center text-center">
-              <span class="skill-icons--javascript"></span> <span>Javascript</span>
-              </div>
-              <div className="text-black d-grid justify-content-center align-items-center text-center">
-              <span class="skill-icons--react-light"></span> <span>React.Js</span>
-              </div>
-              <div className="text-black d-grid justify-content-center align-items-center text-center">
-              <span class="skill-icons--bootstrap"></span> <span>BootTrap</span>
-              </div>
-              <div className="text-black d-grid justify-content-center align-items-center text-center">
-              <span class="skill-icons--tailwindcss-light"></span> <span>Tailwind CSS</span>
-              </div>
-              <div className="text-black d-grid justify-content-center align-items-center text-center">
-              <span class="skill-icons--github-light"></span> <span>Github</span>
-              </div>
-              <div className="text-black d-grid justify-content-center align-items-center text-center">
-              <span class="skill-icons--wordpress"></span> <span>Wordpress</span>
-              </div>
-              <div className="text-black d-grid justify-content-center align-items-center text-center">
-              <span class="skill-icons--figma-light"></span> <span>Figma</span>
-              </div>
+      <div className="section bg-dark bg-gradient shadow-lg text-center text-light p-5">
+        <h1 className="display-6">My SKills</h1>
+        <h6 className="display-6 px-lg-5 fs-6">I utilize my creativity and technical expertise to solve problems with attention to detail, ensuring effective communication and fostering adaptability and team work, all while maintaining a keen understanding of user experience and aesthetics.</h6>
+      </div>
+      <div className="skills bg-dark d-grid align-content-center">
+        <div class="wrapper pb-5">
+          <div class="marquee text-white">
+            <div class="marquee__group">
+                <span className="skill-icons--html"><h6 className="">HTML 5</h6></span>
+                <span class="skill-icons--css"><h6 className="">CSS 3</h6></span>
+                <span class="skill-icons--javascript"><h6 className="">JAVASCRIPT</h6></span>
+                <span class="skill-icons--react-dark"><h6 className="">REACT</h6></span>
+                <span class="skill-icons--vite-dark"><h6 className="">VITE</h6></span>
+                <span class="skill-icons--bootstrap"><h6 className="">BOOTSTRAP</h6></span>
+                <span class="skill-icons--tailwindcss-dark"><h6 className="">TAILWIND</h6></span>
+                <span class="skill-icons--github-dark"><h6 className="">GITHUB</h6></span>
+                <span class="logos--nodejs"><h6 className="">NODE JS</h6></span>
+                <span class="skill-icons--wordpress"><h6 className="">WORDPRESS</h6></span>
+                <span class="skill-icons--figma-dark"><h6 className="">FIGMA</h6></span>
+            </div>
+
+            <div aria-hidden="true" class="marquee__group">
+                <span className="skill-icons--html "><h6 className="">HTML 5</h6></span>
+                <span class="skill-icons--css"><h6 className="">CSS 3</h6></span>
+                <span class="skill-icons--javascript"><h6 className="">JAVASCRIPT</h6></span>
+                <span class="skill-icons--react-dark"><h6 className="">REACT</h6></span>
+                <span class="skill-icons--vite-dark"><h6 className="">VITE</h6></span>
+                <span class="skill-icons--bootstrap"><h6 className="">BOOTSTRAP</h6></span>
+                <span class="skill-icons--tailwindcss-dark"><h6 className="">TAILWIND</h6></span>
+                <span class="skill-icons--github-dark"><h6 className="">GITHUB</h6></span>
+                <span class="logos--nodejs"><h6 className="">NODE JS</h6></span>
+                <span class="skill-icons--wordpress"><h6 className="">WORDPRESS</h6></span>
+                <span class="skill-icons--figma-dark"><h6 className="">FIGMA</h6></span>
+            </div>
+          </div>
+
+          <div class="marquee marquee--reverse">
+            <div class="marquee__group">
+                <span className="skill-icons--html"><h6 className="">HTML 5</h6></span>
+                <span class="skill-icons--css"><h6 className="">CSS 3</h6></span>
+                <span class="skill-icons--javascript"><h6 className="">JAVASCRIPT</h6></span>
+                <span class="skill-icons--react-dark"><h6 className="">REACT</h6></span>
+                <span class="skill-icons--vite-dark"><h6 className="">VITE</h6></span>
+                <span class="skill-icons--bootstrap"><h6 className="">BOOTSTRAP</h6></span>
+                <span class="skill-icons--tailwindcss-dark"><h6 className="">TAILWIND</h6></span>
+                <span class="skill-icons--github-dark"><h6 className="">GITHUB</h6></span>
+                <span class="logos--nodejs"><h6 className="">NODE JS</h6></span>
+                <span class="skill-icons--wordpress"><h6 className="">WORDPRESS</h6></span>
+                <span class="skill-icons--figma-dark"><h6 className="">FIGMA</h6></span>
+            </div>
+
+            <div aria-hidden="true" class="marquee__group">
+                <span className="skill-icons--html"><h6 className="">HTML 5</h6></span>
+                <span class="skill-icons--css"><h6 className="">CSS 3</h6></span>
+                <span class="skill-icons--javascript"><h6 className="">JAVASCRIPT</h6></span>
+                <span class="skill-icons--react-dark"><h6 className="">REACT</h6></span>
+                <span class="skill-icons--vite-dark"><h6 className="">VITE</h6></span>
+                <span class="skill-icons--bootstrap"><h6 className="">BOOTSTRAP</h6></span>
+                <span class="skill-icons--tailwindcss-dark"><h6 className="">TAILWIND</h6></span>
+                <span class="skill-icons--github-dark"><h6 className="">GITHUB</h6></span>
+                <span class="logos--nodejs"><h6 className="">NODE JS</h6></span>
+                <span class="skill-icons--wordpress"><h6 className="">WORDPRESS</h6></span>
+                <span class="skill-icons--figma-dark"><h6 className="">FIGMA</h6></span>
             </div>
           </div>
         </div>
-        <div className="">
-          <div class="slider d-flex justify-content-center align-items-center  p4">
-            <div class="slider-items2 d-flex justify-content-center  align-items-center gap-5 text-center">
-              <div className="text-black d-grid justify-content-center align-items-center text-center">
-                <span className="skill-icons--html"></span> <span>HTML 5</span>
-              </div>
-              <div className="text-black d-grid justify-content-center align-items-center text-center">
-              <span class="skill-icons--css"></span> <span>CSS 3</span>
-              </div>
-              <div className="text-black d-grid justify-content-center align-items-center text-center">
-              <span class="skill-icons--javascript"></span> <span>Javascript</span>
-              </div>
-              <div className="text-black d-grid justify-content-center align-items-center text-center">
-              <span class="skill-icons--react-light"></span> <span>React.Js</span>
-              </div>
-              <div className="text-black d-grid justify-content-center align-items-center text-center">
-              <span class="skill-icons--bootstrap"></span> <span>BootTrap</span>
-              </div>
-              <div className="text-black d-grid justify-content-center align-items-center text-center">
-              <span class="skill-icons--tailwindcss-light"></span> <span>Tailwind CSS</span>
-              </div>
-              <div className="text-black d-grid justify-content-center align-items-center text-center">
-              <span class="skill-icons--github-light"></span> <span>Github</span>
-              </div>
-              <div className="text-black d-grid justify-content-center align-items-center text-center">
-              <span class="skill-icons--wordpress"></span> <span>Wordpress</span>
-              </div>
-              <div className="text-black d-grid justify-content-center align-items-center text-center">
-              <span class="skill-icons--figma-light"></span> <span>Figma</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      </div>
     </>
   );
 };
