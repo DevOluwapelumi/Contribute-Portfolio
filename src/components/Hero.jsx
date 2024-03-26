cf8d57a5cc4b64ba86501c061a838f12c9c3205e
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2024-03-26 12:29:49 +0100

    complete project section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,6 +35,7 @@ const Hero = () => {
           </div>
         </div>
       </div>
+      
     </>
   );
 };
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,70 +5,148 @@ const Projects = () => {
   return (
     <>
       <h1 className="text-light text-center pt-5 display-3">Latest Projects</h1>
-      <div className="row d-flex justify-content-center align-items-center p-5 gap- container-fluid">
-        <div className="col-5 w-50">
-          <div className="card ">
+      <div className="row d-flex justify-content-center align-items-center p-5 gap- container-fluid m-0">
+        <div className="col col-lg-6">
+          <div className="card">
             <img className="card__background" src="de.jpg" alt="" />
             <div className="card__content | flow p-4 ">
               <div className="card__content--container | flow">
                 <h2 className="card__title">Paypal Landing Page</h2>
               </div>
-              <button className="card__button">Read more</button>
+              <div className="card__button d-flex align-items-center justify-content-center social">
+                <a href="https://paypalstaticwebclone.netlify.app/">
+                  <i class="fa fa-link " aria-hidden="true"></i>
+                </a>
+                <a href="https://github.com/DeEminentCoder/Paypal-clone">
+                  <i class="fa fa-file-code-o" aria-hidden="true"></i>
+                </a>
+              </div>
             </div>
           </div>
         </div>
-        <div className="col-5 w-50 ">
+        <div className="col col-lg-6">
           <div className="card ">
             <img className="card__background" src="wee.png" alt="" />
             <div className="card__content | flow p-4 ">
               <div className="card__content--container | flow">
-                <h2 className="card__title">Konga Clone</h2>
+                <h2 className="card__title">Konga Clone (E-commerce)</h2>
               </div>
-              <button className="card__button">Read more</button>
+              <div className="card__button d-flex align-items-center justify-content-center social">
+                <a href="https://konga-clone-deeminentcoder.netlify.app/">
+                  <i class="fa fa-link " aria-hidden="true"></i>
+                </a>
+                <a href="https://github.com/DeEminentCoder/Konga-E-commerce-site">
+                  <i class="fa fa-file-code-o" aria-hidden="true"></i>
+                </a>
+              </div>
             </div>
           </div>
         </div>
-        <div className="col-5 w-50">
+        <div className="col col-lg-6">
           <div className="card ">
             <img className="card__background" src="wew.png" alt="" />
             <div className="card__content | flow p-4 ">
               <div className="card__content--container | flow">
                 <h2 className="card__title">Love Calculator App</h2>
               </div>
-              <button className="card__button">Read more</button>
+              <div className="card__button d-flex align-items-center justify-content-center social">
+                <a href="https://love-calc-deeminentcoder.netlify.app/">
+                  <i class="fa fa-link " aria-hidden="true"></i>
+                </a>
+                <a href="https://github.com/DeEminentCoder/Love-Calculator">
+                  <i class="fa fa-file-code-o" aria-hidden="true"></i>
+                </a>
+              </div>
             </div>
           </div>
         </div>
-        <div className="col-5 w-50">
+        <div className="col col-lg-6">
           <div className="card ">
-            <img className="card__background" src="de.jpg" alt="" />
+            <img className="card__background" src="todo.png" alt="" />
             <div className="card__content | flow p-4 ">
               <div className="card__content--container | flow">
-                <h2 className="card__title">ombia</h2>
+                <h2 className="card__title">Task-management App</h2>
               </div>
-              <button className="card__button">Read more</button>
+              <div className="card__button d-flex align-items-center justify-content-center social">
+                <a href="https://taskmanagement-todo.netlify.app/">
+                  <i class="fa fa-link " aria-hidden="true"></i>
+                </a>
+                <a href="https://github.com/DeEminentCoder/Todo-App">
+                  <i class="fa fa-file-code-o" aria-hidden="true"></i>
+                </a>
+              </div>
             </div>
           </div>
         </div>
-        <div className="col-5 w-50">
+        <div className="col col-lg-6">
           <div className="card ">
-            <img className="card__background" src="de.jpg" alt="" />
+            <img className="card__background" src="wea.png" alt="" />
             <div className="card__content | flow p-4 ">
               <div className="card__content--container | flow">
-                <h2 className="card__title">ombia</h2>
+                <h2 className="card__title">Weather App</h2>
               </div>
-              <button className="card__button">Read more</button>
+              <div className="card__button d-flex align-items-center justify-content-center social">
+                <a href="https://simeonweather.netlify.app/">
+                  <i class="fa fa-link " aria-hidden="true"></i>
+                </a>
+                <a href="https://github.com/DeEminentCoder/My-Weather-App">
+                  <i class="fa fa-file-code-o" aria-hidden="true"></i>
+                </a>
+              </div>
             </div>
           </div>
         </div>
-        <div className="col-5 w-50">
+        <div className="col col-lg-6">
           <div className="card ">
-            <img className="card__background" src="de.jpg" alt="" />
+            <img className="card__background" src="conv.png" alt="" />
             <div className="card__content | flow p-4 ">
               <div className="card__content--container | flow">
-                <h2 className="card__title">ombia</h2>
+                <h2 className="card__title">Currency Converter</h2>
               </div>
-              <button className="card__button">Read more</button>
+              <div className="card__button d-flex align-items-center justify-content-center social">
+                <a href="https://currency-converter-deeminentcoder.netlify.app/converter">
+                  <i class="fa fa-link " aria-hidden="true"></i>
+                </a>
+                <a href="https://github.com/DeEminentCoder/Currency-Converter">
+                  <i class="fa fa-file-code-o" aria-hidden="true"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col col-lg-6">
+          <div className="card ">
+            <img className="card__background" src="calc.png" alt="" />
+            <div className="card__content | flow p-4 ">
+              <div className="card__content--container | flow">
+                <h2 className="card__title">Scientific Calculator</h2>
+              </div>
+              <div className="card__button d-flex align-items-center justify-content-center social">
+                <a href="https://scientific-calculator-deeminentcoder.netlify.app/">
+                  <i class="fa fa-link " aria-hidden="true"></i>
+                </a>
+                <a href="https://github.com/DeEminentCoder/Calculator">
+                  <i class="fa fa-file-code-o" aria-hidden="true"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col col-lg-6">
+          <div className="card ">
+            <img className="card__background" src="pig.png" alt="" />
+            <div className="card__content | flow p-4 ">
+              <div className="card__content--container | flow">
+                <h2 className="card__title">Piggyvest clone</h2>
+              </div>
+              <div className="card__button d-flex align-items-center justify-content-center social">
+                <a href="https://piggyvest-staticweb-clone.netlify.app/">
+                  <i class="fa fa-link " aria-hidden="true"></i>
+                </a>
+                <a href="https://github.com/DeEminentCoder/PiggyVest-Site">
+                  <i class="fa fa-file-code-o" aria-hidden="true"></i>
+                </a>
+              </div>
             </div>
           </div>
         </div>
